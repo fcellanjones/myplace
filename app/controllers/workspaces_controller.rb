4f096f88dcbd9cb5eb7a2b5d4db271a07598f480
Francesca Cellan-Jones <f.cellanjones@gmail.com>
2021-01-21 14:43:19 +0000

    workspace model/controller/view  generated

---
+++ app/controllers/workspaces_controller.rb
@@ -0,0 +1,48 @@
+class WorkspacesController < ApplicationController
+  def index
+    @workspaces = Workspace.all
+  end
+
+  def show
+    @workspace = Workspace.find(params[:id])
+  end
+
+  def new
+    @workspace = Workspace.new
+  end
+
+  def create
+    @workspace = Workspace.create(strong_params)
+    @workspace.user = current_user
+    if @workspace.save
+      redirect_to workspace_path(@workspace)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @workspace = Workspace.find(params[:id])
+  end
+
+  def update
+    @workspace = Workspace.find(params[:id])
+    if @workspace.update(strong_params)
+      redirect_to workspace_path(@workspace)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @workspace = Workspace.find(params[:id])
+    @workspace.destroy
+    redirect_to workspaces_path
+  end
+
+  private
+
+  def strong_params
+    params.require(:workspace).permit(:name, :first_address_line, :postcode, :city, :amenities, :capacity, :prices, :description, amenity_ids: [], photos: [])
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_20_162551) do
+ActiveRecord::Schema.define(version: 2021_01_21_124616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,18 @@ ActiveRecord::Schema.define(version: 2021_01_20_162551) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "workspaces", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "price"
+    t.integer "capacity"
+    t.string "first_line_address"
+    t.string "city"
+    t.string "postcode"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
