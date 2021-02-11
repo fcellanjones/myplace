b2ce9cddabef6428f5026ed0e239844c76e9841c
Francesca Cellan-Jones <f.cellanjones@gmail.com>
2021-02-11 11:09:50 +0000

    uploading photos works

--- app/models/workspace.rb
+++ app/models/workspace.rb
@@ -6,6 +6,7 @@ class Workspace < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :workspace_amenities
   has_many :amenities, through: :workspace_amenities
+  has_many_attached :photos
   geocoded_by :postcode
   after_validation :geocode, if: :will_save_change_to_postcode?
 
