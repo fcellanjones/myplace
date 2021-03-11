400472beaf4a3c72231d26eb971380e629f682b9
Francesca Cellan-Jones <f.cellanjones@gmail.com>
2021-03-11 14:53:21 +0000

    taglines and text added

---
+++ app/javascript/helper_functions/termsConditions.js
@@ -0,0 +1,23 @@
+// const tosCheckbox = document.querySelector('#workspace_terms_of_service');
+// const submitButton = document.querySelector('.btn');
+
+// const checkboxChecked = (input) => {
+  // return input.checked;
+// };
+
+
+// const tosIsValid = checkboxChecked(tosCheckbox);
+ // if (tosIsValid) {
+//     submitButton.disabled = false;
+//   } else {
+//     submitButton.disabled = true;
+//   }
+// };
+
+// allInputs.forEach((input) => {
+//   input.addEventListener('blur', enableButton);
+// });
+// tosCheckbox.addEventListener('click', enableButton);
+
+      // event.preventDefault()}
+// export { termsConditions }
